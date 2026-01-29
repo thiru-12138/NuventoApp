c31a8cb9463c81dad0d88c43e613489ab39338ad
thirumalaiganeshg <thiru.12138@gmail.com>
2026-01-29 20:45:34 +0530

    Add files via upload
    initial setup

---
+++ NuventoMachineTest/NuventoMachineTestApp.swift
@@ -0,0 +1,21 @@
+//
+//  NuventoMachineTestApp.swift
+//  NuventoMachineTest
+//
+//  Created by Thirumalai Ganesh G on 29/01/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct NuventoMachineTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
